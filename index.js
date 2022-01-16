5eea374d2c87ff43936a903891da87f0447c23f4
eridincu <m.erdincoguz@gmail.com>
2022-01-17 02:42:59 +0300

    add env port

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 const server = http.Server(app);
 
 app.use(express.static("public")); // Serve our static assets from /public
-server.listen(8080, () => console.log("Server started"));
+const port = process.env.PORT || 8080;
+server.listen(port, () => console.log("Server started"));
